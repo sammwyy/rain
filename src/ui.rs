df60403197640681a1e4c0770cdc4162d0992222
Sammwy <sammwy.dev@gmail.com>
2023-02-28 08:15:01 -0300

    feature: rendering controls and version

--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,6 @@
 use tui::{
     backend::Backend,
-    layout::{Constraint, Direction, Layout},
+    layout::{Alignment, Constraint, Direction, Layout},
     text::Span,
     widgets::{Block, BorderType, Borders},
     Frame,
@@ -18,12 +18,18 @@ pub fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     // Layout
     let main_chunks = Layout::default()
         .direction(Direction::Vertical)
-        .constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref())
+        .constraints(
+            [
+                Constraint::Percentage(20),
+                Constraint::Percentage(79),
+                Constraint::Min(1),
+            ]
+            .as_ref(),
+        )
         .split(f.size());
 
     let top_chunks = Layout::default()
         .direction(Direction::Horizontal)
-        .margin(1)
         .constraints(
             [
                 Constraint::Percentage(33),
@@ -34,14 +40,21 @@ pub fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
         )
         .split(main_chunks[0]);
 
-    let bottom_chunks = Layout::default()
+    let center_chunks = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([Constraint::Percentage(75), Constraint::Percentage(25)].as_ref())
         .split(main_chunks[1]);
 
+    let bottom_chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(75), Constraint::Percentage(25)].as_ref())
+        .split(main_chunks[2]);
+
     let top_left = top_chunks[0];
     let top_center = top_chunks[1];
     let top_right = top_chunks[2];
+    let center_left = center_chunks[0];
+    let center_right = center_chunks[1];
     let bottom_left = bottom_chunks[0];
     let bottom_right = bottom_chunks[1];
 
@@ -70,12 +83,24 @@ pub fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
 
     // Proccess list
     let state = &mut app.state.selected_process.clone();
-    f.render_stateful_widget(process_list(&mut app.state), bottom_left, state);
+    f.render_stateful_widget(process_list(&mut app.state), center_left, state);
 
     // Process detail
     let process_detail_area = Block::default()
         .borders(Borders::all())
         .border_type(BorderType::Plain)
         .title(vec![Span::from("Process Details")]);
-    f.render_widget(process_detail_area, bottom_right);
+    f.render_widget(process_detail_area, center_right);
+
+    // Controls
+    let controls_area = Block::default().title(vec![Span::from(
+        "[Q] Quit [1] Name [2] PID [3] RAM [4] CPU [5] DISK",
+    )]);
+    f.render_widget(controls_area, bottom_left);
+
+    // Version
+    let version_area = Block::default()
+        .title_alignment(Alignment::Right)
+        .title(vec![Span::from("Rain v0.1.0")]);
+    f.render_widget(version_area, bottom_right);
 }
